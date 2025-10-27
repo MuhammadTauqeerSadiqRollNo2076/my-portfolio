5c3ba91353e2f0dd01f84af1562f5e3bb25baf89
Muhammad Tauqeer Sadiq <chtouqeerchtouqeermayo@gmail.com>
2025-10-27 22:45:51 +0500

    add tailwind in vite

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss()
+  ],
   server: {
     port: 3000,
     host: true // Mobile mein access karne ke liye
