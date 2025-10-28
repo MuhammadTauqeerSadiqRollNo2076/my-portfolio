f89d3bbb15febae2166545a0978e561fb3f9f10e
Muhammad Tauqeer Sadiq <chtouqeerchtouqeermayo@gmail.com>
2025-10-28 21:38:56 +0500

    first commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,7 +32,7 @@ export default function Contact() {
     setStatus("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://my-backend-green-six.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
